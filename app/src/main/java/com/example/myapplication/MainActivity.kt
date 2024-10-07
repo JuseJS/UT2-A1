f834aabf365b6fdfa4e08ec88cf2bf56a9e316b2
Navarro <joseanp0202@gmail.com>
2024-10-07 12:05:40 +0100

    add WriteReadFile.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,20 +6,16 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import java.io.File
 import java.io.FileInputStream
@@ -53,7 +49,7 @@ class MainActivity : ComponentActivity() {
             verticalArrangement = Arrangement.Center
         ) {
             Button(
-                onClick = { guardarTextoEnArchivo(datetime, nombreArchivo) }
+                onClick = { WriteReadFile.guardarTextoEnArchivo(datetime, nombreArchivo) }
             ) {
                 Text("Guardar archivo")
             }
@@ -62,29 +58,6 @@ class MainActivity : ComponentActivity() {
             ) {
                 Text("Leer archivo")
             }
-        }
-    }
-
-    private fun guardarTextoEnArchivo(texto: String, nombreArchivo: String) {
-        val estadoAlmacenamiento = Environment.getExternalStorageState()
-
-        if (estadoAlmacenamiento == Environment.MEDIA_MOUNTED) {
-            val directorio = getFilesDir()
-            val archivo = File(directorio, nombreArchivo)
-
-            try {
-                val flujoSalida = FileOutputStream(archivo, true)
-                val writer = OutputStreamWriter(flujoSalida)
-                writer.append(texto)
-                writer.close()
-
-                Toast.makeText(this, "Texto añadido en $directorio $nombreArchivo", Toast.LENGTH_SHORT).show()
-            } catch (e: Exception) {
-                e.printStackTrace()
-                Toast.makeText(this, "Error al guardar el archivo", Toast.LENGTH_SHORT).show()
-            }
-        } else {
-            Toast.makeText(this, "No se pudo acceder al almacenamiento externo", Toast.LENGTH_SHORT).show()
         }
     }
 
